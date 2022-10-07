4fc34e23b5d0cf2b72afc6e3006b873a20faaa2d
Anatoly <anatolypanushkin@mail.ru>
2022-10-07 11:28:02 +0300

    Add "NewOwnerMessage" for publish this type in the bus

---
+++ AutoMessages/NewOwnerMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoMessages;
+
+public class NewOwnerMessage
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string VehicleOfOwner { get; set; }
+    public DateTime ListedAtUtc { get; set; }
+}
