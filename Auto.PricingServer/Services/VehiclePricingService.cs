0867af1f26efbf10d43f2a67dc71d0858dd90fa2
Anatoly <anatolypanushkin@mail.ru>
2022-11-08 16:14:35 +0300

    Added Pricing Server and Client

---
+++ Auto.PricingServer/Services/VehiclePricingService.cs
@@ -0,0 +1,20 @@
+﻿using Auto.PricingService;
+using Grpc.Core;
+
+namespace Auto.PricingServer.Services;
+
+public class VehiclePricingService : Pricer.PricerBase
+{
+    private readonly ILogger<VehiclePricingService> _logger;
+
+    public VehiclePricingService(ILogger<VehiclePricingService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<PriceReply> GetPrice(PriceRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new PriceReply() {CurrencyCode = "RUB", Price = 400});
+    }
+    
+}
