154235b4e2f6dbd1fc9e9abe7a3a73ccd0870546
Anatoly <anatolypanushkin@mail.ru>
2022-11-13 13:56:01 +0300

    Add SignalR Hub and Client

--- Auto.Website/Startup.cs
+++ Auto.Website/Startup.cs
@@ -53,7 +53,9 @@ namespace Auto.Website {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
                 app.UseGraphQLAltair();
+                app.UseExceptionHandler("/Home/Error");
             } else {
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -67,7 +69,9 @@ namespace Auto.Website {
             app.UseSwaggerUI();
             app.UseGraphQL<ISchema>();
 
-            app.UseEndpoints(endpoints => {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<AutoHub>("/hub");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
